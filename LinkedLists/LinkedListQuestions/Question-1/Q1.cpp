ac04e463ba773cb5d8e785d3c323428cc851afa1
Shubham Thakur <shubhamthakur@example.com>
2026-02-09 09:30:19 +0530

    revers a doubly linkedlist

--- LinkedLists/LinkedListQuestions/Question-1/Q1.cpp
+++ LinkedLists/LinkedListQuestions/Question-1/Q1.cpp
@@ -33,3 +33,36 @@ public:
         return prev;
     }
 };
+
+// Reverse a Doubly Linked List
+// Difficulty: Easy
+// You are given the head of a doubly linked list. You have to reverse the doubly linked list and return its head.
+
+// Examples:
+
+// Input:
+
+// Output: 5 <-> 4 <-> 3
+// Explanation: After reversing the given doubly linked list the new list will be 5 <-> 4 <-> 3.
+
+class Solution
+{
+public:
+    Node *reverse(Node *head)
+    {
+        // code here
+        Node *curr = head;
+        Node *prev = NULL;
+
+        while (curr != NULL)
+        {
+            Node *forw = curr->next;
+            curr->next = prev;
+            curr->prev = forw;
+            prev = curr;
+            curr = forw;
+        }
+
+        return prev;
+    }
+};
