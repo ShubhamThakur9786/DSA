b5b8246e673a724761a530e30c5ee82cb0265596
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-02-28 13:22:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (12.41%) - LeetSync

---
+++ 983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        if(pushed.size() != popped.size()) {
+            return false;
+        }
+        int n = pushed.size();
+        stack<int> st;
+        int j = 0;
+        for(int i=0; i<n; i++) {
+            st.push(pushed[i]);
+            while(!st.empty() && st.top() == popped[j]) {
+                st.pop();
+                j++;
+            }
+        }
+
+        if(st.empty()) {
+            return true;
+        }
+        return false;
+    }
+};
