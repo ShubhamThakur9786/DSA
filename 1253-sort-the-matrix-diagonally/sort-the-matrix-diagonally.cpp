ec245a5fa7c714c7e27f2bf3843945fe9e6299db
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-05-04 21:46:58 +0530

    Time: 5 ms (24.40%) | Memory: 13.8 MB (40.15%) - LeetSync

---
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        unordered_map<int, vector<int>> mp;
+
+        int m = mat.size();
+        int n = mat[0].size();
+        for(int i=0; i<mat.size(); i++) {
+            for(int j=0; j<mat[i].size(); j++) {
+                mp[i-j].push_back(mat[i][j]);
+            }             
+        }
+
+        for(auto &it : mp) {
+            sort(it.second.begin(), it.second.end());
+        }
+
+        for(int i=m-1; i>=0; i--) {
+            for(int j=n-1; j>=0; j--) {
+                mat[i][j] = mp[i-j].back();
+                mp[i-j].pop_back();
+            }
+        }
+
+        return mat;
+    }
+};
