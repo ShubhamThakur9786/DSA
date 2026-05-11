92da28dc193df56cf34e95448bdf4020338a384c
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-05-11 23:59:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (11.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,6 +4,13 @@ public:
         if(n <= 1) {
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        //bottom up approch
+        vector<int> dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
