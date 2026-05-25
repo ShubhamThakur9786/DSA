f1a5e508279ad39618fbbb17105911ae80dde8e7
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-05-25 23:12:23 +0530

    Time: 7 ms (78.74%) | Memory: 23.8 MB (54.22%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+         int n = s.size();
+
+        queue<int> q;
+        q.push(0);
+
+        int farthest = 0;
+
+        while (!q.empty()) {
+            int idx = q.front();
+            q.pop();
+
+            int start = max(idx + minJump, farthest);
+            int end = min(idx + maxJump, n - 1);
+
+            for (int j = start; j <= end; j++) {
+                if (s[j] == '0') {
+
+                    if (j == n - 1) {
+                        return true;
+                    }
+
+                    q.push(j);
+                }
+            }
+
+            farthest = end + 1;
+        }
+
+        return n == 1;
+    }
+};
