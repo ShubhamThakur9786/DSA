eeaa29743c9d51e9df9489e770d4402e962cf019
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-05-31 11:49:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (95.27%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,48 +1,75 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> st1;
-        stack<char> st2;
-
-        for(int i=0; i<s.size(); i++) {
-            if(s[i] != '#') {
-                st1.push(s[i]);
-            }
-            else {
-                if(!st1.empty()) {
-                    st1.pop();
-                }
-            }
-        }
-
         string str1 = "";
-        while(!st1.empty()) {
-            str1.push_back(st1.top());
-            st1.pop();
-        }
+        string str2 = "";
 
-        for(int i=0; i<t.size(); i++) {
-            if(t[i] != '#') {
-                st2.push(t[i]);
+        for(auto c1 : s) {
+            if(c1 != '#') {
+                str1.push_back(c1);
             }
             else {
-                if(!st2.empty()) {
-                    st2.pop();
+                if(str1.size() >= 1) {
+                    str1.pop_back();
+                }
+            }
+        }
+        for(auto c2 : t) {
+            if(c2 != '#') {
+                str2.push_back(c2);
+            }
+            else {
+                if(str2.size() >= 1) {
+                    str2.pop_back();
                 }
             }
         }
 
-        string str2 = "";
-        while(!st2.empty()) {
-            str2.push_back(st2.top());
-            st2.pop();
-        }
-
-        if(str1 == str2) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return str1 == str2;
     }
 };
+
+
+        // stack<char> st1;
+        // stack<char> st2;
+
+        // for(int i=0; i<s.size(); i++) {
+        //     if(s[i] != '#') {
+        //         st1.push(s[i]);
+        //     }
+        //     else {
+        //         if(!st1.empty()) {
+        //             st1.pop();
+        //         }
+        //     }
+        // }
+
+        // string str1 = "";
+        // while(!st1.empty()) {
+        //     str1.push_back(st1.top());
+        //     st1.pop();
+        // }
+
+        // for(int i=0; i<t.size(); i++) {
+        //     if(t[i] != '#') {
+        //         st2.push(t[i]);
+        //     }
+        //     else {
+        //         if(!st2.empty()) {
+        //             st2.pop();
+        //         }
+        //     }
+        // }
+
+        // string str2 = "";
+        // while(!st2.empty()) {
+        //     str2.push_back(st2.top());
+        //     st2.pop();
+        // }
+
+        // if(str1 == str2) {
+        //     return true;
+        // }
+        // else {
+        //     return false;
+        // }
