f4b8aef8d529565edc63beb27c83613813fcfda6
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-06-07 00:31:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (43.22%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -12,23 +12,47 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-       vector<vector<int>> ans;
-       if(root == NULL) return ans;
-       queue<TreeNode*> q;
-       q.push(root);
-
-       while(!q.empty()) {
-        int size = q.size();
-        vector<int> level;
-        for(int i=0; i<size; i++) {
-            TreeNode* temp = q.front();
-            q.pop();
-            level.push_back(temp->val);
-            if(temp->left) q.push(temp->left);
-            if(temp->right) q.push(temp->right);
+        vector<vector<int>> ans;
+        if(!root) {
+            return ans;
         }
-        ans.push_back(level);
-       }
-       return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()) {
+            int len = q.size();
+            vector<int> level;
+            for(int i=0; i<len; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+                level.push_back(temp->val);
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+            ans.push_back(level);
+        }
+        return ans;
     }
 };
+
+
+
+
+    //    vector<vector<int>> ans;
+    //    if(root == NULL) return ans;
+    //    queue<TreeNode*> q;
+    //    q.push(root);
+
+    //    while(!q.empty()) {
+    //     int size = q.size();
+    //     vector<int> level;
+    //     for(int i=0; i<size; i++) {
+    //         TreeNode* temp = q.front();
+    //         q.pop();
+    //         level.push_back(temp->val);
+    //         if(temp->left) q.push(temp->left);
+    //         if(temp->right) q.push(temp->right);
+    //     }
+    //     ans.push_back(level);
+    //    }
+    //    return ans;
