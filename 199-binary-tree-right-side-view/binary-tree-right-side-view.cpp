01c39309f93e88b6fbe0246dccd774f369bcf2e7
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-06-14 09:12:42 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (22.44%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -9,21 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //iterative approch
 class Solution {
 public:
-    void solve(TreeNode* &root, vector<int> &ans, int level) {
-        if(root == NULL) return;
-        if(level == ans.size()) {
-            ans.push_back(root->val);
-        }
-        solve(root->right, ans, level+1);
-        solve(root->left, ans, level+1);
-    }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
         if(!root) return ans;
-        int level = 0;
-        solve(root, ans, level);
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> temp;
+            for(int i=0; i<size; i++) {
+                TreeNode* tp = q.front();
+                q.pop();
+                temp.push_back(tp->val);
+                if(tp->left) q.push(tp->left);
+                if(tp->right) q.push(tp->right);
+            }
+
+            ans.push_back(temp[temp.size()-1]);
+        }
         return ans;
     }
 };
+
+//recursive approch 
+
+// class Solution {
+// public:
+//     void solve(TreeNode* &root, vector<int> &ans, int level) {
+//         if(root == NULL) return;
+//         if(level == ans.size()) {
+//             ans.push_back(root->val);
+//         }
+//         solve(root->right, ans, level+1);
+//         solve(root->left, ans, level+1);
+//     }
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int> ans;
+//         if(!root) return ans;
+//         int level = 0;
+//         solve(root, ans, level);
+//         return ans;
+//     }
+// };
