a01985cde97c7cb6908238a5f294715ad624bd8f
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-06-16 13:13:24 +0530

    Time: 4 ms (63.64%) | Memory: 39.9 MB (42.05%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    string duplicate(string result) {
-        string ans = "";
-        for(int i=0; i<result.size(); i++) {
-            ans += result[i];
-        }
-        return ans;
-    }
-    void reverse(string &result) {
-        int i=0;
-        int j=result.size()-1;
-        while(i <= j) {
-            swap(result[i], result[j]);
-            i++;
-            j--;
-        }
-    }
     string processStr(string s) {
         // * -> remove last char
         // # -> duplicates current res and append it to result
@@ -32,11 +16,10 @@ public:
                 }
             }
             else if(s[i] == '#') {
-                string dupres = duplicate(result);
-                result += dupres;
+                result += result;
             }
             else {
-                reverse(result);
+                reverse(result.begin(), result.end());
             }
         }
         return result;
