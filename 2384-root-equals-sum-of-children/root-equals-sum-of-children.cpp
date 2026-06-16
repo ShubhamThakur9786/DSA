c283b74fe9d423696c288d4a8b995b17bf1f8a6f
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-06-16 09:19:20 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (92.09%) - LeetSync

--- 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
@@ -14,21 +14,9 @@ public:
     bool checkTree(TreeNode* root) {
         if(!root) return true;
 
-        if(!root->left && !root->right) {
-            return true;
-        }
-        int sum = 0;
-        if(root->left) {
-            sum += root->left->val;
-        }
-        if(root->right) {
-            sum += root->right->val;
-        }
-
-        if(sum != root->val) {
+        if(root->val != root->left->val + root->right->val) {
             return false;
         }
-
-        return checkTree(root->left) && checkTree(root->right);
+        return true;
     }
 };
