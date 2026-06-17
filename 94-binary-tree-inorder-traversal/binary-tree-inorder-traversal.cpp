b07f96b5a616af15c74fe19be74ec7dcd57f6125
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-06-17 16:00:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (88.82%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,38 +11,73 @@
  */
 class Solution {
 public:
+    // void inOrder(TreeNode* root, vector<int> &ans) {
+    //     if(root == NULL) {
+    //         return;
+    //     }
+    //     inOrder(root->left, ans);
+    //     ans.push_back(root->val);
+    //     inOrder(root->right, ans);
+    // }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
         if(!root) return ans;
-        stack<bool> vis;
-        stack<TreeNode*> st;
-        st.push(root);
-        vis.push(0);
+        //moris traversal
 
-        while(!st.empty()) {
-            TreeNode* temp = st.top();
-            st.pop();
-            bool flag = vis.top();
-            vis.pop();
-            if(!flag) {
-                //right node push
-                if(temp->right) {
-                    st.push(temp->right);
-                    vis.push(0);
-                }
-                //root node push
-                st.push(temp);
-                vis.push(1);
-                //left node push
-                if(temp->left) {
-                    st.push(temp->left);
-                    vis.push(0);
-                }
+        while(root) {
+            if(!root->left) {
+                ans.push_back(root->val);
+                root = root->right;
             }
             else {
-                ans.push_back(temp->val);
+                TreeNode* curr = root->left;
+                while(curr->right && curr->right != root) {
+                    curr = curr->right;
+                }
+                if(curr->right == NULL) {
+                    curr->right = root;
+                    root = root->left;
+                }
+                else {
+                    curr->right = NULL;
+                    ans.push_back(root->val);
+                    root = root->right;
+                }
             }
         }
         return ans;
     }
 };
+
+        // vector<int> ans;
+        // if(!root) return ans;
+        // stack<bool> vis;
+        // stack<TreeNode*> st;
+        // st.push(root);
+        // vis.push(0);
+
+        // while(!st.empty()) {
+        //     TreeNode* temp = st.top();
+        //     st.pop();
+        //     bool flag = vis.top();
+        //     vis.pop();
+        //     if(!flag) {
+        //         //right node push
+        //         if(temp->right) {
+        //             st.push(temp->right);
+        //             vis.push(0);
+        //         }
+        //         //root node push
+        //         st.push(temp);
+        //         vis.push(1);
+        //         //left node push
+        //         if(temp->left) {
+        //             st.push(temp->left);
+        //             vis.push(0);
+        //         }
+        //     }
+        //     else {
+        //         ans.push_back(temp->val);
+        //     }
+        // }
+        // return ans;
