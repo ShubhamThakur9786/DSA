7219d3db5fd96878b6311395b9959f1a75519142
Shubham Thakur <shubhamthakur9786@gmail.com>
2026-07-17 16:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (44.33%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
@@ -10,22 +10,79 @@
 class Solution {
 public:
     int guessNumber(int n) {
-        int l = 0;
-        int e = n;
-        while(l <= e) {
-            int guess_no = l+(e-l)/2;
-            int val = guess(guess_no);
+        int st = 1;
+        int end = n;
 
-            if(val == 0) {
-                return guess_no;
+        while(st <= end) {
+            int mid = st + (end-st)/2;
+            if(guess(mid) == 0) {
+                return mid;
             }
-            else if(val == -1) {
-                e = guess_no-1;
+            else if(guess(mid) == -1) {
+                end = mid-1;
             }
             else {
-                l = guess_no+1;
+                st = mid+1;
             }
         }
-        return -1;
+        return 0;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /** 
+//  * Forward declaration of guess API.
+//  * @param  num   your guess
+//  * @return 	     -1 if num is higher than the picked number
+//  *			      1 if num is lower than the picked number
+//  *               otherwise return 0
+//  * int guess(int num);
+//  */
+
+// class Solution {
+// public:
+//     int guessNumber(int n) {
+//         int l = 0;
+//         int e = n;
+//         while(l <= e) {
+//             int guess_no = l+(e-l)/2;
+//             int val = guess(guess_no);
+
+//             if(val == 0) {
+//                 return guess_no;
+//             }
+//             else if(val == -1) {
+//                 e = guess_no-1;
+//             }
+//             else {
+//                 l = guess_no+1;
+//             }
+//         }
+//         return -1;
+//     }
+// };
